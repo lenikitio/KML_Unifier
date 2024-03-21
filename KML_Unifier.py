d60381b7c92bc8620f504393b8716b0a90812836
Operator <Operator@fieldnote-38.geoscan.lan>
2024-03-22 00:19:34 +0300

    Second commit

--- KML_Unifier.py
+++ KML_Unifier.py
@@ -1,4 +1,6 @@
 from os import listdir
+import os
+from zipfile import ZipFile
 # Объединение полётов
 with open("Полёты.kml", 'w', encoding= 'utf-8') as data:
     data.write('<?xml version="1.0" encoding="UTF-8"?>\n'
@@ -26,22 +28,25 @@ with open("Полёты.kml", 'w', encoding= 'utf-8') as data:
     
 
     # Объединение КТ из swmaps
-with open("КТ_и_ПБС.kml", 'w', encoding= 'utf-8') as data:
-    data.write('<?xml version="1.0" encoding="UTF-8"?>\n'
+with open("КТ_и_ПБС.kml", 'w', encoding= 'utf-8') as kt:
+    kt.write('<?xml version="1.0" encoding="UTF-8"?>\n'
                '<kml xmlns="http://www.opengis.net/kml/2.2">\n'
                '  <Document>\n'
                '    <name>КТ и ПБС</name>\n'
-               '      <open>0</open>\n'
-               '        <description>Exported using SW Maps</description>'
-               '          <Folder>')
-    for file in listdir('Отчеты/KML'):
-        if file.endswith(".kml"):
-                    flight = 'Отчеты/KML/' + file
-                    with open(flight, 'r', encoding= 'utf-8') as kml:
-                           lines = kml.readlines()
-                           data.writelines(lines[13:-4])
+               '    <open>0</open>\n'
+               '    <description>Exported using SW Maps</description>\n'
+               '      <Folder>\n')
+    for date in listdir('PVP_data/'):
+        for group in listdir('PVP_data/' + date):
+                for file in listdir('PVP_data/' + date + '/' + group + '/Ground_Photo/Export'):
+                    if file.endswith(".kmz"):
+                        zip_point = 'PVP_data/' + date + '/' + group + '/Ground_Photo/Export/' + file
+                        with ZipFile(zip_point, 'r') as temp_zip:
+                            temp_zip.extractall('temp_file') 
+                        with open('temp_file/doc.kml', 'r', encoding= 'utf-8') as kml:
+                            lines = kml.readlines()
+                            kt.writelines(lines[8: -3])
 
-                           
-    data.write('    </Folder>\n'
+    kt.write('    </Folder>\n'
                '  </Document>\n'
                '</kml>')
